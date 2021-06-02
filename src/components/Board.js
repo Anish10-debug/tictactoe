ca8520a094a117278f03efe8a8a981aeae01890c
Anish10-debug <agbhat@mitaoe.ac.in>
2021-06-02 12:44:05 +0530

    created board and square components

---
+++ src/components/Board.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Squares from './Squares';
+//3 divs for 3 rows and 3 buttons in each row
+const Board = () => {
+  return (
+    <div>
+      <div>
+        <Squares value={0} />
+        <Squares value={1} />
+        <Squares value={2} />
+      </div>
+
+      <div>
+        <Squares value={3} />
+        <Squares value={4} />
+        <Squares value={5} />
+      </div>
+
+      <div>
+        <Squares value={6} />
+        <Squares value={7} />
+        <Squares value={8} />
+      </div>
+    </div>
+  );
+};
+
+export default Board;
---
+++ src/components/Squares.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Squares = ({ value }) => {
+  return <button type="button">{value}</button>;
+};
+
+export default Squares;
